128feb28dd6f7b285910a32ed8a82f85e69ea837
dusandjergovic <149064873+dusandjergovic@users.noreply.github.com>
2024-02-01 14:13:44 +0100

    Uradjen domaci za objekte

---
+++ script.js
@@ -0,0 +1,11 @@
+        // DOMACI
+        var korpa = {
+            'ssd': 500,
+            'maticna ploca': 450
+        };
+        console.log(korpa);
+        
+        korpa.gpu = 500;
+        korpa.napajanje = 300;
+        korpa.ram = 100;
+        console.log(korpa);
